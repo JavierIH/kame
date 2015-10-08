8b730336eb935968603ea1d69080b7e2d3f51079
JavierIH <javier.i.h@hotmail.com>
2015-10-08 22:24:05 +0000

    basic walking

--- code/kame.py
+++ code/kame.py
@@ -1,6 +1,6 @@
 import os
 import sys
-sys.path.append(os.path.abspath(__file__).replace('kame/code/main.py', '') + 'pybotics/')
+sys.path.append(os.path.abspath(__file__).replace('kame/code/kame.py', '') + 'pybotics/')
 
 import time
 import smbus
@@ -11,7 +11,7 @@ from scipy import signal
 
 class Kame(object):
     
-    def __init__(self, servo_trims, servo_pins, i2c_bus=0, pca9685_address=0x40, name='kame'):
+    def __init__(self, servo_trims, servo_pins, i2c_bus=1, pca9685_address=0x40, name='kame'):
         
         #Configuration
         self._name = name
@@ -30,31 +30,28 @@ class Kame(object):
         #Setting up OctoSnake
         self.osc = []
         self.osc.append(octosnake.Oscillator())
-        self.osc.append(octosnake.Oscillator(octosnake.semiSin))
         self.osc.append(octosnake.Oscillator())
-        self.osc.append(octosnake.Oscillator(octosnake.semiSin))
+        self.osc.append(octosnake.Oscillator())
         self.osc.append(octosnake.Oscillator())
 
         self.osc[1].ref_time = self.osc[0].ref_time
         self.osc[2].ref_time = self.osc[0].ref_time
-        self.osc[3].ref_time = self.osc[0].ref_time
-        self.osc[4].ref_time = self.osc[0].ref_time
 
         #Setting up servo controller
         for i in range(len(self._servo_pins)):
             self.controller.addServo(self._servo_pins[i], self._servo_trims[i])
 
-        self.controllerler.servos[self._servo_pins[1]].reverse = True
+        self.controller.servos[self._servo_pins[1]].reverse = True
         self.controller.servos[self._servo_pins[3]].reverse = True
 
 
     def walk(self, steps):
 
-        T = 900                 #milliseconds 
-        period = [T, T, T, T, T]
-        amplitude = [left_x_amp, z_amp, right_x_amp, z_amp, swing_amp]
-        offset = [-10, -75, -10, -75, 0]
-        phase = [90, 180, 270, 0, 335+90]
+        T = 4000                 #milliseconds 
+        period = [T, T, T, 10000]
+        amplitude = [40, 40, 30, 0]
+        offset = [0, 0, 0, 0]
+        phase = [0, 180, 90, 0]
 
         for i in range(len(self.osc)):
             self.osc[i].period = period[i]
@@ -68,7 +65,71 @@ class Kame(object):
                 for i in range(len(self.osc)):
                     self.osc[i].refresh()
 
-                self.controller.move(self._servo_pins[0], osc[0].output[0])
+                self.controller.move(self._servo_pins[0], self.osc[0].output+15)
+                self.controller.move(self._servo_pins[1], self.osc[1].output+15)
+                self.controller.move(self._servo_pins[2], self.osc[1].output+30)
+                self.controller.move(self._servo_pins[3], self.osc[0].output+30)
+                self.controller.move(self._servo_pins[4], self.osc[2].output)
+                self.controller.move(self._servo_pins[5], self.osc[2].output)
+                self.controller.move(self._servo_pins[6], self.osc[2].output)
+                self.controller.move(self._servo_pins[7], self.osc[2].output)
+
 
             except IOError:
                 self._bus = smbus.SMBus(self._i2c_bus)
+
+    def walk_borrico(self, steps):
+
+        T = 800                 #milliseconds 
+        period = [T, T, T, T]
+        amplitude = [30, 30, 40, 40]
+        offset = [0, 0, 0, 0]
+        phase = [0, 180, 90, 270]
+
+        for i in range(len(self.osc)):
+            self.osc[i].period = period[i]
+            self.osc[i].amplitude = amplitude[i]
+            self.osc[i].phase = phase[i]
+            self.osc[i].offset = offset[i]
+
+        final = time.time() + float(T*steps/1000)
+        while time.time() < final:
+            try:
+                for i in range(len(self.osc)):
+                    self.osc[i].refresh()
+
+                self.controller.move(self._servo_pins[0], self.osc[0].output)
+                self.controller.move(self._servo_pins[1], self.osc[0].output)
+                self.controller.move(self._servo_pins[2], self.osc[1].output)
+                self.controller.move(self._servo_pins[3], self.osc[1].output)
+                self.controller.move(self._servo_pins[4], self.osc[2].output)
+                self.controller.move(self._servo_pins[5], self.osc[3].output)
+                self.controller.move(self._servo_pins[6], self.osc[2].output)
+                self.controller.move(self._servo_pins[7], self.osc[3].output)
+
+
+            except IOError:
+                self._bus = smbus.SMBus(self._i2c_bus)
+
+
+    def zero(self):
+        for i in range(len(self._servo_pins)):
+            self.controller.move(self._servo_pins[i], 0)
+            
+			
+
+    def jump(self):
+        for i in range(len(self._servo_pins)):
+            self.controller.move(self._servo_pins[i], 0)
+
+        self.controller.move(self._servo_pins[4], 30)
+        self.controller.move(self._servo_pins[5], -30)
+        self.controller.move(self._servo_pins[6], 30)
+        self.controller.move(self._servo_pins[7], -30)
+        time.sleep(2.5)
+
+        self.controller.move(self._servo_pins[4], -50)
+        self.controller.move(self._servo_pins[5], 50)
+        self.controller.move(self._servo_pins[6], -50)
+        self.controller.move(self._servo_pins[7], 50)
+
